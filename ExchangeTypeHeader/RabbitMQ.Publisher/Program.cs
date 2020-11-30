d53fe27e3010b118c0f8495ad9d65a327ea3097b
orhanrecep90 <46787760+orhanrecep90@users.noreply.github.com>
2020-12-01 01:01:57 +0300

    Add files via upload

---
+++ ExchangeTypeHeader/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Info = 3,
+        Warning = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = int.Parse(args[1]);
+            int count = 20; 
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps:");
+            //for local RabbitMq installation
+            //factory.HostName = "localhost";
+
+            using (var connection=factory.CreateConnection())
+            {   
+                using (var channel=connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("header", durable:true,type:ExchangeType.Headers);
+                    var properties = channel.CreateBasicProperties();
+
+                    Dictionary<string, object> headers = new Dictionary<string, object>();
+                    headers.Add("format", "pdf");
+                    headers.Add("shape", "a4");
+
+                    channel.BasicPublish("header", "", properties, Encoding.UTF8.GetBytes("My message"));
+
+                    Console.WriteLine("message send!");
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+
+
+
